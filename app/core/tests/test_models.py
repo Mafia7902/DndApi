3d01c14cfd387fed3eb86e52f794ac05f4f50198
Kevin Van Wyk <kevin.vanwyk@4-sure.net>
2022-02-17 10:37:40 +0200

    Normalized email and Created Superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,4 @@
+import email
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -6,8 +7,8 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@londonappdev.com'
-        password = 'Password123'
+        email = 'test@gmail.com'
+        password = 'password'
         user = get_user_model().objects.create_user(
 			email=email,
 			password=password
@@ -15,3 +16,27 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    
+    def test_normalization_of_email(self):
+        """Test to see if the email becomes normalized"""
+        email = "testmail@GMAIL.Com"
+        password = "password123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_create_superuser_successful(self):
+        """Test if the creation of superuser uses email"""
+        email = "test@gmail.com"
+        password = "password"
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
